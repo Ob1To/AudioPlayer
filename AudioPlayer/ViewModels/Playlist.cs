a4db28e31e918afce0e9ca4121c0f8943edd8f4e
Eleonora Dimova <eleonoradimova94@gmail.com>
2015-08-20 17:29:35 +0300

    new files

---
+++ AudioPlayer/ViewModels/Playlist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer.ViewModels
+{
+    public class Playlist
+    {
+        private string playListName;
+        public string PlaylistName
+        {
+            get
+            {
+                return this.playListName;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    this.playListName = value;
+                }
+            }
+        }
+
+        private string playListFileName;
+        public string PlayListFileName
+        {
+            get
+            {
+                return this.playListFileName;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    this.playListFileName = value;
+                }
+            }
+        }
+    }
+}
